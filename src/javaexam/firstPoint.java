46359d6d1d0101a952a2b45c14073b18dd1f817f
AtanasTs2525 <81189898+AtanasTs2525@users.noreply.github.com>
2023-10-22 09:37:11 +0300

    test second

---
+++ src/javaexam/firstPoint.java
@@ -0,0 +1,10 @@
+package javaexam;
+
+public class firstPoint {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
